c76beabef2b7d828eefd917ec8ba78a243be5919
raducu427 <32168993+raducu427@users.noreply.github.com>
2018-09-23 12:40:43 +0300

    Update tictactoe.hs

--- tictactoe.hs
+++ tictactoe.hs
@@ -26,16 +26,16 @@ initGame = Game (setElem cursor (1,1) $ matrix 3 3 $ \(i,j) -> empty) empty (1,1
 
 move :: Char -> State Game () 
 move key = do
-  matrix        <- use gameMatrix
   pos           <- use position
-  currentPlayer <- use player 
+  matrix        <- use gameMatrix
   prevElem      <- use prevElement 
-  let newPos c | c == 'd' = moveCursor MoveRight
+  currentPlayer <- use player 
+  let c                   = toLower key
+      newMatrix           = setElem currentPlayer pos matrix
+      newPos c | c == 'd' = moveCursor MoveRight
                | c == 'a' = moveCursor MoveLeft
                | c == 's' = moveCursor MoveDown
                | c == 'w' = moveCursor MoveUp 
-      newMatrix = setElem currentPlayer pos matrix 
-      c = toLower key
   if c `elem` ['d','a','s','w'] then do
     prevElement .= uncurry getElem (newPos c pos) matrix     
     gameMatrix  %= (setElem cursor (newPos c pos))
@@ -45,7 +45,7 @@ move key = do
     gameMatrix  .= newMatrix 
     status      .= checkGameStatus pos newMatrix currentPlayer
     prevElement .= currentPlayer
-    player      %= switchPlayer               
+    player      %= (\currentPlayer -> if currentPlayer == playerX then playerO else playerX)               
   else return ()    
    where
      moveCursor dir pos = case dir of
@@ -57,25 +57,21 @@ move key = do
        let antiDiagonal matrix = V.generate 3 $ \i -> matrix!(i + 1, 3 - i)
            antiTrace matrix = V.sum (antiDiagonal matrix)
        in if (V.sum (getRow (pos^._1) matrix) * V.sum (getCol (pos^._2) matrix) * trace matrix * antiTrace matrix) `mod` currentPlayer == 0 
-          then winner currentPlayer else if all (> empty) (toList matrix) then Draw else Playing 
-     winner player = if player == playerX then PlayerXwon else PlayerOwon  
-     switchPlayer currentPlayer = if currentPlayer == playerX then playerO else playerX      
+          then (\player -> if player == playerX then PlayerXwon else PlayerOwon ) currentPlayer 
+          else if all (> empty) (toList matrix) then Draw else Playing    
 
 render :: Game -> IO ()
 render game = do  
-  clearScreen
-  hideCursor
-  printGrid (game^.gameMatrix)
-  replicateM_ 3 (putChar '\n')
+  clearScreen >> hideCursor >> printGrid (game^.gameMatrix) >> replicateM_ 3 (putChar '\n')
   case game^.status of
     Playing    -> (if (game^.player) == playerX then putStr "player's X turn" else putStr "player's O turn") >> cursorUpLine 8
     PlayerXwon -> putStr "player X won"      
     PlayerOwon -> putStr "player O won" 
     Draw       -> putStr "draw" 
    where
-     printGrid = zipWithM_ zipper [1..] . toList 
-     zipper i e    = prinContent e >> if i `mod` 3 == 0 then putChar '\n' >> printDashes 5 else putChar '|'        
-     printDashes n = replicateM_ n (putChar '-') >> putChar '\n'   
+     printGrid                    = zipWithM_ zipper [1..] . toList 
+     zipper i e                   = prinContent e >> if i `mod` 3 == 0 then putChar '\n' >> printDashes 5 else putChar '|'        
+     printDashes n                = replicateM_ n (putChar '-') >> putChar '\n'   
      prinContent e | e == playerX = putChar 'X'
                    | e == playerO = putChar 'O'
                    | e == cursor  = putChar '*'
